df8912f82737cf6cb18b96c780e15e46f3834320
inditang-iw <indi.tang@infinityworks.com>
2024-05-03 14:00:17 +0100

    refactor & add docstrings

--- rewrite_news.py
+++ rewrite_news.py
@@ -1,33 +1,81 @@
 from openai import OpenAI
 from headline import Headline
 
-# create an instance of Headline
-headline = Headline(api_key='1de03f53-a39f-4573-84b6-36a504d6d23a', section='uk-news')
+class NewsRewriter:
+    """
+    This module provides functionality to rewrite news articles in a format suitable for kids aged 11-14 to read in 5 minutes.
+    """
+
+    def __init__(self, api_key, section):
+        """
+        Initializes the NewsRewriter object.
+
+        Args:
+            api_key (str): The API key for the Headline API.
+            section (str): The section of the news to retrieve.
+
+        Returns:
+            None
+        """
+        self.headline = Headline(api_key=api_key, section=section)
+
+    def get_article_web_url(self):
+        """
+        Retrieves the web URL of the article.
+
+        Returns:
+            str: The web URL of the article.
+        """
+        return self.headline.get_article_web_url()
+
+    def get_article_text(self):
+        """
+        Retrieves the text of the article.
+
+        Returns:
+            str: The text of the article.
+        """
+        return self.headline.get_article_text()
+
+    def rewrite_article_for_kids(self):
+        """
+        Rewrites the article in a format suitable for kids aged 11-14 to read in 5 minutes.
+
+        Returns:
+            str: The rewritten article.
+        """
+        article_text = self.get_article_text()
+        role_prompt = "You are a school teacher"
+        rewrite_prompt = "Rewrite the following article in a format suitable for kids aged 11-14 to read in 5 minutes: " + article_text
+
+        client = OpenAI(
+            organization='org-6nwmJPLFhoVcsTHUI4tUAAGE',
+            project='proj_oa45HVI0HZksNfOpJcaAFH4N',
+            api_key='sk-proj-cpJ2hlLE3YFgAPr0ITFNT3BlbkFJwUPINQZjmJGijJMWrq9O'
+        )
+
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": role_prompt},
+                {"role": "user", "content": rewrite_prompt},
+            ]
+        )
+
+        return completion.choices[0].message.content
+
+
+# create an instance of NewsRewriter
+news_rewriter = NewsRewriter(api_key='1de03f53-a39f-4573-84b6-36a504d6d23a', section='uk-news')
 
 # get article web url
-article_web_url = headline.get_article_web_url()
-print("webUrl: {}\n\n".format(article_web_url))
+article_web_url = news_rewriter.get_article_web_url()
+print(f"webUrl: {article_web_url}\n\n")
+
 # get article text
-article_text = headline.get_article_text()
-print("Article text: {}\n\n".format(article_text))
+article_text = news_rewriter.get_article_text()
+print(f"Article text: {article_text}\n\n")
 
-# create a prompt for the OpenAI API
-role_prompt = "You are a school teacher"
-rewrite_prompt = "Rewrite the following article in a format suitable for kids aged 11-14 to read in 5 minutes: " + article_text
-
-# call openai api to generate a summary of the article text
-client = OpenAI(
-    organization='org-6nwmJPLFhoVcsTHUI4tUAAGE',
-    project='proj_oa45HVI0HZksNfOpJcaAFH4N',
-    api_key='sk-proj-cpJ2hlLE3YFgAPr0ITFNT3BlbkFJwUPINQZjmJGijJMWrq9O'
-)
-
-# Call ChatGPT API to generate a summary of article_text
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": role_prompt},
-        {"role": "user", "content": rewrite_prompt},
-    ]
-)
-print(completion.choices[0].message.content)
+# rewrite the article for kids
+rewritten_article = news_rewriter.rewrite_article_for_kids()
+print(rewritten_article)
